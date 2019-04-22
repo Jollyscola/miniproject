005f85cb61f633fa67e125a6ad5875df4d4e3d57
Jonas Elbro <cph-je98@cphbusiness.dk>
2019-04-22 17:42:58 +0200

    final

--- settings.js
+++
@@ -1,9 +0,0 @@
-const DEV_DB_URI = "mongodb+srv://Jollys:Morfar2019@kerstin-kjdhl.mongodb.net/mini_dev";
-const TEST_DB_URI = "mongodb+srv://Jollys:Morfar2019@kerstin-kjdhl.mongodb.net/mini_test";
-const MOCHA_TEST_TIMEOUT = 5000;
-
-module.exports = {
- DEV_DB_URI,
- TEST_DB_URI,
- MOCHA_TEST_TIMEOUT
-}
